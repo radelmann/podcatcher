da6af8dfa8ec8a21677507ee53177868d724076f
radelmann <robadelmann@gmail.com>
2016-03-28 11:31:52 -0700

    wired up add items to queue and basic displaying of queue items

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import { routeReducer as routing } from 'redux-simple-router';
 import podcasts from './podcasts';
 import player from './player';
+import queue from './queue';
 
-export default combineReducers({routing, podcasts, player});
+export default combineReducers({routing, podcasts, player, queue});
--- src/views/queue.js
+++ src/views/queue.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as podcastActions from  '../action-creators/podcasts';
 
-export default class Queue extends Component {
-  render() {
-    console.log('playlist loaded');
+class Queue extends Component {
+  renderItem(itemData) {
     return (
-      <div>this is the queue content</div>      
+      <div>{itemData.title}</div>
+    );
+  }
+
+  render() {
+    console.log('queue loaded');
+    return (
+      <div>this is the queue content
+        { this.props.queue.map(this.renderItem) }      
+      </div>
     );
   }
 }
+
+function mapStateToProps (state) {
+  return { queue: state.queue }; 
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(podcastActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Queue);
