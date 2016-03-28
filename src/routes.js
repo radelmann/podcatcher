dd54d19b7f9ae6335a97de6f855cf9e32dbc8e9c
radelmann <robadelmann@gmail.com>
2016-03-28 10:47:59 -0700

    renamed playlist to queue

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import { Router, IndexRoute, Route } from 'react-router';
 import App from './views/app';
 import PodcastList from './views/podcast-list';
 import PodcastEpisodeList from './views/podcast-episode-list';
-import Playlist from './views/playlist';
+import Queue from './views/queue';
 
 const Routes = (
   <Route path="/" component={App}>
     <IndexRoute component={PodcastList} />
-    <Route path="playlist" component={Playlist} />
+    <Route path="queue" component={Queue} />
     <Route path=":podcastId" component={PodcastEpisodeList} />
   </Route>
 );
