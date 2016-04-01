9c6b33a14c5349883ec18fdcd0acc24309580bac
radelmann <robadelmann@gmail.com>
2016-04-01 08:58:19 -0700

    add queue reordering funcionality with drag and drop

--- src/reducers/queue.js
+++ src/reducers/queue.js
@@ -1,4 +1,4 @@
-import {ADD_EPISODE_TO_QUEUE, REMOVE_EPISODE_FROM_QUEUE} from '../actions';
+import {MOVE_QUEUE_ITEM, ADD_EPISODE_TO_QUEUE, REMOVE_EPISODE_FROM_QUEUE} from '../actions';
 import {createReducer} from '../utils';
 
 const addEpisodeToQueue = (state, {payload}) =>
@@ -7,9 +7,26 @@ const addEpisodeToQueue = (state, {payload}) =>
 const removeEpisodeFromQueue = (state, {payload}) => 
   state.filter(item => item !== payload)
 
+//move queue item from one index to another index
+//adjust indexes accordingly
+const moveQueueItem = (state, {payload}) => {
+  console.log(payload);
+  var newState = state.slice();
+  if (payload.to >= newState.length) {
+      var k = payload.to - newState.length;
+      while ((k--) + 1) {
+          newState.push(undefined);
+      }
+  }
+  newState.splice(payload.to, 0, newState.splice(payload.from, 1)[0]);
+  return newState;
+}
+  
+
 const handlers = {
   [ADD_EPISODE_TO_QUEUE]: addEpisodeToQueue,
-  [REMOVE_EPISODE_FROM_QUEUE]: removeEpisodeFromQueue
+  [REMOVE_EPISODE_FROM_QUEUE]: removeEpisodeFromQueue,
+  [MOVE_QUEUE_ITEM]: moveQueueItem
 };
 
 export default createReducer({}, handlers);
--- src/views/queue.js
+++ src/views/queue.js
@@ -2,30 +2,57 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as podcastActions from  '../action-creators/podcasts';
+import Sortable from 'sortablejs';
 
 class Queue extends Component {
   constructor(props) {
     super(props);
 
     this.renderItem = this.renderItem.bind(this);
+    this.sortableGroupDecorator = this.sortableGroupDecorator.bind(this);
+    this.sortEnd = this.sortEnd.bind(this);
+  }
+  
+  sortEnd (evt) {
+    this.props.moveQueueItem({ from:evt.oldIndex, to:evt.newIndex});
   }
 
-  renderItem(ep) {
+  sortableGroupDecorator (componentBackingInstance) {
+    // check if backing instance not null
+    if (componentBackingInstance) {
+      let options = {
+        draggable: "li", // Specifies which items inside the element should be sortable
+        group: "shared",
+        onEnd: this.sortEnd
+      };
+      Sortable.create(componentBackingInstance, options);
+    }
+  }
+
+  renderItem(ep, i) {
     return (
-      <li >{ep.title} - <a onClick={ () => this.props.removeEpisodeFromQueue(ep) }> remove episode</a>
+      <li key={i}>{ep.title} - <a onClick={ () => this.props.removeEpisodeFromQueue(ep) }> remove episode</a>
       </li>
     );
   }
 
   render() {
-    return (
-      <div>
-        <h3>Podcast Queue</h3>
-        <ol>
-          { this.props.queue.map(this.renderItem) }      
-        </ol>
-      </div>
-    );
+    if (Array.isArray(this.props.queue) && (this.props.queue.length>0)) { 
+      return (
+        <div>
+          <h3>Podcast Queue (Drag items to reorder)</h3>
+          <ol className="group-list" ref={this.sortableGroupDecorator}>
+            { this.props.queue.map(this.renderItem) }      
+          </ol>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Podcast Queue (Empty)</h3>
+        </div>
+      );
+    }
   }
 }
 
